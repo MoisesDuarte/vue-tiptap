efda9372ca8195f98258ceaa0df19b0703e7487a
MoisesDuarte <moisesduartejunior@gmail.com>
2021-06-23 08:47:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-tiptap
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-tiptap)
---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css" rel="stylesheet">
+
+    <AppTextEditor v-model="content" />
+    <button 
+      class="submit-button" 
+      @click="onSubmit">
+      Submit
+    </button>
+  </div>
+</template>
+
+<script>
+import AppTextEditor from './components/AppTextEditor';
+
+export default {
+  name: 'App',
+  components: {
+    AppTextEditor,
+  },
+  data() {
+    return {
+      content: '',
+    };
+  },
+  methods: {
+    onSubmit() {
+      alert(JSON.stringify(this.content));
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+#app {
+  font-family: Arial;
+  font-size: 16px;
+
+  .submit-button {
+    border: 1px solid #333;
+    border-radius: 2px;
+    background: #fff;
+    color: #333;
+    font-size: 16px;
+    padding: 6px 12px;
+    margin-top: 8px;
+    -webkit-appearance: none;
+    cursor: pointer;
+
+    &:hover {
+      background: #333;
+      color: #fff;
+    }
+  }
+}
+</style>
